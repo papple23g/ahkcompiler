81b123cf30d974274513d5399c6a614b7a0ff1a9
papple23g <papple23g@gmail.com>
2021-03-27 17:25:35 +0800

    [Fix] ngrok

--- templates/ahkblockly.py
+++ templates/ahkblockly.py
@@ -5041,7 +5041,7 @@ def DownloadAhkExe(ev):
     global countdown_timer,sec_int
     #host="http://127.0.0.1:8001"
     ##!!
-    host="https://cb6aec9a8719.ngrok.io"
+    host="https://6c946b26ec8c.ngrok.io"
     btn_elt=ev.currentTarget
 
     #停用按鍵
